2051cac2eec24ea824a12fbde1698956549a05a7
Noizee <edwitter123magejo@gmail.com>
2026-01-06 08:55:58 +0200

    Fix: Add DB file for Vercel and Update CSP

--- next.config.ts
+++ next.config.ts
@@ -9,7 +9,7 @@ const nextConfig: NextConfig = {
         headers: [
           {
             key: 'Content-Security-Policy',
-            value: "default-src 'self'; script-src 'self' 'unsafe-eval' 'unsafe-inline'; style-src 'self' 'unsafe-inline'; img-src 'self' blob: data: https:; font-src 'self' data: https://r2cdn.perplexity.ai; connect-src 'self' https:;",
+            value: "default-src 'self' https://vercel.live; script-src 'self' 'unsafe-eval' 'unsafe-inline' https://vercel.live; style-src 'self' 'unsafe-inline'; img-src 'self' blob: data: https:; font-src 'self' data: https://r2cdn.perplexity.ai; connect-src 'self' https: wss://*.vercel.live;",
           },
         ],
       },
